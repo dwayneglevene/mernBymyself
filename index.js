ffa30fef3fbe3ca8e41dc3d9acc178cecb9d7051
Dwayne Levene <39275855+dwayneglevene@users.noreply.github.com>
2022-11-03 18:33:24 -0700

    understanding mongo 2

--- index.js
+++ index.js
@@ -11,8 +11,22 @@ const mongodb = require('mongodb');
 
 mongodb.connect(process.env.CONNECTIONSTRING, {useUnifiedTopology:true},async function(err, client){
 const db = client.db()
-const results = await db.collection("Users").find().toArray();
-console.log(results)
+
+// read
+// const results = await db.collection("Users").find({Name:"Jason"}).toArray();
+const students = db.collection("Users")
+
+//create
+// await students.insertOne({Name:"Jenny",School:"Barton"})
+
+//update
+// await students.updateOne({_id:mongodb.ObjectId("636467b9729ee74e9c853475") }, {$set:{Name:"Rae"}})
+// console.log("Added new student")
+ 
+
+// delete
+await students.deleteOne({_id:mongodb.ObjectId("636467b9729ee74e9c853475") })
+
 client.close()
 
 })

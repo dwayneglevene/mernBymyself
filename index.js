6be19db37169b927f677b7a28f376f3cafc88403
Dwayne Levene <39275855+dwayneglevene@users.noreply.github.com>
2022-11-03 17:53:56 -0700

    understanding mongo

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ dotenv.config()
 
 
 const mongodb = require('mongodb');
-const { mongo } = require('mongoose');
+// const { mongo } = require('mongoose');
 
 
 
-const connectionstring = "mongodb+srv:/Dwayne:mongodb+srv://<username>:<password>@cluster0.m3b5r9h.mongodb.net/?retryWrites=true&w=majority@cluster0.m3b5r9h.mongodb.net/?retryWrites=true&w=majority"
 
 
-mongodb.connect(process.env.CONNECTIONSTRING, function(err, client){
-
-    // const db = client.db()
-    // const results = await db.collection("pets").find().toArray()
-    // console.log(results)
+mongodb.connect(process.env.CONNECTIONSTRING, {useUnifiedTopology:true},async function(err, client){
+const db = client.db()
+const results = await db.collection("Users").find().toArray();
+console.log(results)
+client.close()
 
 })
 
